1a70d803af800d0a008dc206b6e2a46d4b660879
Kevin Goslar <kevin.goslar@gmail.com>
2021-12-14 22:12:06 -0600

    main.rs mod.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 use std::env;
-use std::process::Command;
 
 mod runnables;
 
 fn main() {
-    if let Some(mut command) = find_command(env::args()) {
+    if let Some(mut command) = runnables::find(env::args()) {
         let status = command.status().unwrap();
         std::process::exit(status.code().unwrap());
     } else {
         println!("No command to execute found");
         std::process::exit(1);
     }
-}
-
-/// provides the command to run
-fn find_command(args: env::Args) -> Option<Command> {
-    if let Some(dir) = runnables::makefile::detect() {
-        return Some(runnables::makefile::command(args, dir));
-    }
-    None
 }
