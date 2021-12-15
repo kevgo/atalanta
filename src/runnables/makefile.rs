878050dcdcd7d7b6eebf514d193bb684e286b0a3
Kevin Goslar <kevin.goslar@gmail.com>
2021-12-14 21:18:54 -0600

    makefile.rs

--- src/runnables/makefile.rs
+++ src/runnables/makefile.rs
@@ -1,17 +1,24 @@
 use super::Runnable;
-use crate::UserError;
 use std::env;
 use std::path::PathBuf;
 
 /// detects a Makefile in the current or a parent directory
-pub fn detect() -> Result<Box<dyn Runnable>, UserError> {
-    let mut dir = env::current_dir().expect("Cannot get current directory");
-    Ok(Box::new(MakefileRunnable { dir }))
+pub fn detect() -> Option<Box<dyn Runnable>> {
+    let dir = env::current_dir().expect("Cannot get current directory");
+    loop {
+        let mut path = dir.join("Makefile");
+        if path.is_file() {
+            return Some(Box::new(MakefileRunnable { path }));
+        }
+        if !path.pop() || !path.pop() {
+            return None;
+        }
+    }
 }
 
 pub struct MakefileRunnable {
     /// path of the Makefile
-    dir: PathBuf,
+    path: PathBuf,
 }
 
 impl Runnable for MakefileRunnable {
