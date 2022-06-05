8902a7fc3daf64d19541dd1bd576c06b74f21569
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-05 15:57:57 -0500

    Node (yarn) (#8)

--- src/probes/node_npm.rs
+++ src/probes/node_npm.rs
@@ -23,21 +23,31 @@ impl Stack for NodeNpmStack {
 }
 
 #[derive(Deserialize)]
-struct PackageJson {
-    scripts: HashMap<String, String>,
+pub struct PackageJson {
+    pub scripts: HashMap<String, String>,
 }
 
 pub fn scan(stacks: &mut Stacks) {
     if !Path::new("package-lock.json").exists() {
         return;
     }
+    let package_json = match load_package_json() {
+        Some(file) => file,
+        None => return,
+    };
+    stacks.push(Box::new(NodeNpmStack {
+        tasks: parse_scripts(package_json),
+    }));
+}
+
+pub fn load_package_json() -> Option<PackageJson> {
     let file = match File::open("package.json") {
         Ok(file) => file,
         Err(e) => match e.kind() {
-            ErrorKind::NotFound => return,
+            ErrorKind::NotFound => return None,
             e => {
                 println!("Warning: Cannot read file \"package.json\": {}", e);
-                return;
+                return None;
             }
         },
     };
@@ -49,12 +59,10 @@ pub fn scan(stacks: &mut Stacks) {
                 "Warning: file \"package.json\" has an invalid structure: {}",
                 e
             );
-            return;
+            return None;
         }
     };
-    stacks.push(Box::new(NodeNpmStack {
-        tasks: parse_scripts(package_json),
-    }));
+    Some(package_json)
 }
 
 fn parse_scripts(package_json: PackageJson) -> Vec<Task> {
---
+++ src/probes/node_yarn.rs
@@ -0,0 +1,47 @@
+use super::node_npm::{load_package_json, PackageJson};
+use crate::{Stack, Stacks, Task};
+use std::fmt::Display;
+use std::path::Path;
+
+pub struct NodeYarnStack {
+    tasks: Vec<Task>,
+}
+
+impl Display for NodeYarnStack {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Node.JS (yarn)")
+    }
+}
+
+impl Stack for NodeYarnStack {
+    fn tasks(&self) -> &Vec<Task> {
+        &self.tasks
+    }
+}
+
+pub fn scan(stacks: &mut Stacks) {
+    if !Path::new("yarn.lock").exists() {
+        return;
+    }
+    let package_json = match load_package_json() {
+        Some(file) => file,
+        None => return,
+    };
+    stacks.push(Box::new(NodeYarnStack {
+        tasks: parse_scripts(package_json),
+    }));
+}
+
+fn parse_scripts(package_json: PackageJson) -> Vec<Task> {
+    let mut result = vec![];
+    for (key, value) in package_json.scripts {
+        result.push(Task {
+            name: key.clone(),
+            cmd: "yarn".into(),
+            argv: vec!["--silent".into(), "run".into(), key],
+            desc: Some(value),
+        });
+    }
+    result.sort();
+    result
+}
