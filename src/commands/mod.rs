db47db61dfe0c29f0c53a1ab757c305452a5bf1b
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-05 06:29:58 -0500

    mod.rs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,7 @@
 use crate::probes::Stack;
 use crate::Outcome;
 use std::io::Write;
+use std::str;
 use tabwriter::TabWriter;
 
 /// lists all available commands
@@ -11,9 +12,9 @@ pub fn list(stack: impl Stack) -> Outcome {
         let text = format!("{}\t{}", task.cmd, task.desc.as_deref().unwrap_or_default());
         tw.write(text.as_bytes()).unwrap();
     }
-    println!(
-        "{}",
-        std::str::from_utf8(tw.into_inner().unwrap().as_ref()).unwrap()
-    );
+    let bytes = tw.into_inner().unwrap();
+    unsafe {
+        println!("{}", str::from_utf8_unchecked(&bytes));
+    }
     Outcome::Ok
 }
