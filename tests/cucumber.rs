1c35a8f29ec30dd84202ce10ea6fd2699f4801e0
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-05 11:15:02 -0500

    cucumber.rs

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -119,11 +119,10 @@ fn tmp_dir() -> PathBuf {
     dir
 }
 
-fn create_file<P1: AsRef<Path>, P2: AsRef<Path>>(filename: P1, content: &str, dir: P2) {
+fn create_file<P1: AsRef<Path>>(filename: P1, content: &str, dir: &Path) {
     let filename = filename.as_ref();
-    let dir = dir.as_ref();
     if let Some(parent) = filename.parent() {
-        fs::create_dir_all(&dir.join(parent)).unwrap();
+        fs::create_dir_all(dir.join(parent)).unwrap();
     }
     let mut file = File::create(&dir.join(filename)).unwrap();
     file.write_all(content.as_bytes()).unwrap();
