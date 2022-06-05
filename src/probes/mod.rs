53ad9d4f228b84e95327c6729146298cbccbe941
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-05 15:50:28 -0500

    node_yarn.feature mod.rs node_yarn.rs

--- src/probes/mod.rs
+++ src/probes/mod.rs
@@ -2,11 +2,13 @@ use crate::Stacks;
 
 mod makefile;
 mod node_npm;
+mod node_yarn;
 
 /// determines the stacks in the current directory
 pub fn scan() -> Stacks {
     let mut result = vec![];
     makefile::scan(&mut result);
     node_npm::scan(&mut result);
+    node_yarn::scan(&mut result);
     result
 }
---
+++ src/probes/node_yarn.rs
@@ -0,0 +1,72 @@
+use crate::{Stack, Stacks, Task};
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufReader, ErrorKind};
+use std::path::Path;
+
+pub struct NodeYarnStack {
+    tasks: Vec<Task>,
+}
+
+impl Display for NodeYarnStack {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("Node.JS (yarn)")
+    }
+}
+
+impl Stack for NodeYarnStack {
+    fn tasks(&self) -> &Vec<Task> {
+        &self.tasks
+    }
+}
+
+#[derive(Deserialize)]
+struct PackageJson {
+    scripts: HashMap<String, String>,
+}
+
+pub fn scan(stacks: &mut Stacks) {
+    if !Path::new("yarn.lock").exists() {
+        return;
+    }
+    let file = match File::open("package.json") {
+        Ok(file) => file,
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => return,
+            e => {
+                println!("Warning: Cannot read file \"package.json\": {}", e);
+                return;
+            }
+        },
+    };
+    let reader = BufReader::new(file);
+    let package_json: PackageJson = match serde_json::from_reader(reader) {
+        Ok(content) => content,
+        Err(e) => {
+            println!(
+                "Warning: file \"package.json\" has an invalid structure: {}",
+                e
+            );
+            return;
+        }
+    };
+    stacks.push(Box::new(NodeYarnStack {
+        tasks: parse_scripts(package_json),
+    }));
+}
+
+fn parse_scripts(package_json: PackageJson) -> Vec<Task> {
+    let mut result = vec![];
+    for (key, value) in package_json.scripts {
+        result.push(Task {
+            name: key.clone(),
+            cmd: "yarn".into(),
+            argv: vec!["--silent".into(), "run".into(), key],
+            desc: Some(value),
+        });
+    }
+    result.sort();
+    result
+}
