194010da5235cc106b1939eab6fd27bdc9089b76
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-05 20:22:22 -0500

    setup.rs

--- src/commands/setup.rs
+++ src/commands/setup.rs
@@ -14,17 +14,16 @@ pub fn setup(workspace: Workspace) -> Outcome {
             .stdout(Stdio::inherit())
             .stderr(Stdio::inherit())
             .output();
-        match output {
-            Ok(output) => {
-                if let Some(exit_code) = output.status.code() {
-                    if exit_code != 0 {
-                        return Outcome::ScriptFailed {
-                            exit_code: exit_code as u8,
-                        };
-                    }
+        let output = match output {
+            Ok(output) => output,
+            Err(e) => return Outcome::CannotFindExecutable { err: e.to_string() },
+        };
+        if let Some(exit_code) = output.status.code() {
+            if exit_code != 0 {
+                return Outcome::ScriptFailed {
+                    exit_code: exit_code as u8,
                 };
             }
-            Err(e) => return Outcome::CannotFindExecutable { err: e.to_string() },
         };
     }
     if executed {
