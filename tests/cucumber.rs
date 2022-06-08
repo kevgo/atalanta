8d7954a3e46b3bde599004646676327ab47a1cc6
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-08 06:44:20 -0500

    t

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -1,168 +1,51 @@
+use std::{convert::Infallible, time::Duration};
+
 use async_trait::async_trait;
-use cucumber::gherkin::Step;
-use cucumber::{given, then, when, World, WorldInit};
-use itertools::Itertools;
-use rand::Rng;
-use std::borrow::Cow;
-use std::convert::Infallible;
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::{Path, PathBuf};
-use std::process::{Command, Output};
-use std::str;
-use std::time::{SystemTime, UNIX_EPOCH};
+use cucumber::{given, then, when, WorldInit};
+use tokio::time::sleep;
 
 #[derive(Debug, WorldInit)]
-pub struct RunWorld {
-    /// the directory containing the test files of the current scenario
-    pub dir: PathBuf,
-
-    /// the result of running Atlanta
-    pub output: Option<Output>,
+struct World {
+    user: Option<String>,
+    capacity: usize,
 }
 
 #[async_trait(?Send)]
-impl World for RunWorld {
+impl cucumber::World for World {
     type Error = Infallible;
 
-    async fn new() -> Result<Self, Infallible> {
+    async fn new() -> Result<Self, Self::Error> {
         Ok(Self {
-            dir: tmp_dir(),
-            output: None,
+            user: None,
+            capacity: 0,
         })
     }
 }
 
-impl RunWorld {
-    /// provides the exit code of the Atlanta run
-    fn exit_code(&self) -> i32 {
-        match &self.output {
-            Some(output) => output.status.code().unwrap(),
-            None => panic!(),
-        }
-    }
+#[given(expr = "{word} is hungry")] // Cucumber Expression
+async fn someone_is_hungry(w: &mut World, user: String) {
+    sleep(Duration::from_secs(2)).await;
 
-    /// provides the textual output of the Atlanta run
-    fn output(&self) -> Cow<str> {
-        match &self.output {
-            Some(output) => String::from_utf8_lossy(&output.stdout),
-            None => Default::default(),
-        }
-    }
-
-    /// provides the textual output of the Atlanta run with whitespace trimmed from every line
-    fn output_trimmed(&self) -> String {
-        self.output()
-            .trim()
-            .lines()
-            .map(|line| line.trim_end())
-            .join("\n")
-    }
+    w.user = Some(user);
 }
 
-#[given(regex = r#"^a file "(.+)" with content:$"#)]
-fn a_file_with_content(world: &mut RunWorld, step: &Step, filename: String) {
-    let content = step.docstring.as_ref().unwrap().trim();
-    create_file(filename, content, &world.dir)
+#[when(regex = r"^(?:he|she|they) eats? (\d+) cucumbers?$")]
+async fn eat_cucumbers(w: &mut World, count: usize) {
+    sleep(Duration::from_secs(2)).await;
+
+    w.capacity += count;
+
+    assert!(w.capacity < 4, "{} exploded!", w.user.as_ref().unwrap());
 }
 
-#[given(regex = r#"^a file "(.+)"$"#)]
-fn a_file(world: &mut RunWorld, filename: String) {
-    create_file(filename, "", &world.dir)
+#[then("she is full")]
+async fn is_full(w: &mut World) {
+    sleep(Duration::from_secs(2)).await;
+
+    assert_eq!(w.capacity, 3, "{} isn't full!", w.user.as_ref().unwrap());
 }
 
-#[given("a Makefile with content:")]
-fn a_makefile(world: &mut RunWorld, step: &Step) {
-    let content = step.docstring.as_ref().unwrap().trim();
-    let tabulized = convert_to_makefile_format(content);
-    create_file("Makefile", &tabulized, &world.dir)
-}
-
-#[when(regex = r#"^executing "(.*)"$"#)]
-fn executing(world: &mut RunWorld, command: String) {
-    let mut argv = command.split_ascii_whitespace();
-    match argv.next() {
-        Some("a") => {}
-        _ => panic!("The end-to-end tests can only run the 'a' command for now"),
-    }
-    world.output = Some(
-        Command::new("../../target/debug/a")
-            .args(argv)
-            .current_dir(&world.dir)
-            .output()
-            .expect("cannot find the 'a' executable"),
-    );
-}
-
-#[then("it prints:")]
-fn verify_output(world: &mut RunWorld, step: &Step) {
-    let want = step.docstring.as_ref().unwrap().trim();
-    let stripped = strip_ansi_escapes::strip(world.output_trimmed()).unwrap();
-    let have = str::from_utf8(&stripped).unwrap();
-    assert_eq!(have, want);
-}
-
-#[then(regex = "^the exit code is (\\d)$")]
-fn exit_code(world: &mut RunWorld, want: i32) {
-    assert_eq!(world.exit_code(), want);
-}
-
-#[then(regex = "^the output contains \"(.*)\"$")]
-fn output_contains(world: &mut RunWorld, text: String) {
-    assert!(world.output().contains(&text));
-}
-
-#[then(regex = "^the workspace contains a folder \"(.*)\"$")]
-fn contains_folder(world: &mut RunWorld, folder: String) {
-    assert!(world.dir.join(folder).is_dir())
-}
-
-fn main() {
-    // You may choose any executor you like (`tokio`, `async-std`, etc.).
-    // You may even have an `async` main. Cucumber is composable.
-    futures::executor::block_on(RunWorld::run("features"));
-}
-
-/// creates a temporary directory
-fn tmp_dir() -> PathBuf {
-    let timestamp = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis();
-    let rand: String = rand::thread_rng()
-        .sample_iter(&rand::distributions::Alphanumeric)
-        .take(3)
-        .map(char::from)
-        .collect();
-    let cwd = env::current_dir().expect("cannot determine the current directory");
-    let dir = cwd.join("tmp").join(format!("{}-{}", timestamp, rand));
-    fs::create_dir_all(&dir).unwrap();
-    dir
-}
-
-fn create_file<P1: AsRef<Path>>(filename: P1, content: &str, dir: &Path) {
-    let filename = filename.as_ref();
-    if let Some(parent) = filename.parent() {
-        fs::create_dir_all(dir.join(parent)).unwrap();
-    }
-    let mut file = File::create(&dir.join(filename)).unwrap();
-    file.write_all(content.as_bytes()).unwrap();
-}
-
-/// this codebase uses 2 spaces for indentation but Makefiles require tabs
-fn convert_to_makefile_format(text: &str) -> String {
-    let mut result = String::new();
-    for line in text.lines() {
-        if line.starts_with("  ") {
-            result.push('\t');
-            result.push_str(&line[2..]);
-            result.push('\n');
-        } else {
-            result.push_str(line);
-            result.push('\n');
-        }
-    }
-    result
+#[tokio::main]
+async fn main() {
+    World::run("tests/features/readme").await;
 }
