e5e2eea08ceb545b6c7ccdf17e1ab905aa3c360a
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-08 06:39:40 -0500

    stack.rs task.rs workspace.rs

--- src/domain/workspace.rs
+++ src/domain/workspace.rs
@@ -8,7 +8,7 @@ impl Workspace {
     /// helps find tasks by name
     pub fn task_with_name(&self, name: &str) -> Option<&Task> {
         for stack in &self.stacks {
-            if let Some(task) = stack.task_with_name(name) {
+            if let Some(task) = stack.task_matching_name(name) {
                 return Some(task);
             }
         }
