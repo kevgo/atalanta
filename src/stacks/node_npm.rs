d5a1ba9669224ac533f98ac47e6f9ae1409ecb63
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-09 16:44:24 -0500

    Indent by two spaces

--- src/stacks/node_npm.rs
+++ src/stacks/node_npm.rs
@@ -9,75 +9,75 @@ use std::path::Path;
 use std::process::Command;
 
 struct NodeNpmStack {
-    tasks: Vec<Task>,
+  tasks: Vec<Task>,
 }
 
 impl Display for NodeNpmStack {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Node.JS (npm)")
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.write_str("Node.JS (npm)")
+  }
 }
 
 impl Stack for NodeNpmStack {
-    fn setup(&self) -> Option<Command> {
-        let mut command = Command::new("npm");
-        command.arg("install");
-        Some(command)
-    }
+  fn setup(&self) -> Option<Command> {
+    let mut command = Command::new("npm");
+    command.arg("install");
+    Some(command)
+  }
 
-    fn tasks(&self) -> &Vec<Task> {
-        &self.tasks
-    }
+  fn tasks(&self) -> &Vec<Task> {
+    &self.tasks
+  }
 }
 
 #[derive(Deserialize)]
 pub struct PackageJson {
-    pub scripts: HashMap<String, String>,
+  pub scripts: HashMap<String, String>,
 }
 
 pub fn scan(stacks: &mut Stacks) {
-    if !Path::new("package-lock.json").exists() {
-        return;
-    }
-    let Some(package_json) = load_package_json() else {
-        return;
-    };
-    stacks.push(Box::new(NodeNpmStack {
-        tasks: parse_scripts(package_json),
-    }));
+  if !Path::new("package-lock.json").exists() {
+    return;
+  }
+  let Some(package_json) = load_package_json() else {
+    return;
+  };
+  stacks.push(Box::new(NodeNpmStack {
+    tasks: parse_scripts(package_json),
+  }));
 }
 
 pub fn load_package_json() -> Option<PackageJson> {
-    let file = match File::open("package.json") {
-        Ok(file) => file,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => return None,
-            e => {
-                println!("Warning: Cannot read file \"package.json\": {e}");
-                return None;
-            }
-        },
-    };
-    let reader = BufReader::new(file);
-    match serde_json::from_reader(reader) {
-        Ok(content) => Some(content),
-        Err(e) => {
-            println!("Warning: file \"package.json\" has an invalid structure: {e}",);
-            None
-        }
+  let file = match File::open("package.json") {
+    Ok(file) => file,
+    Err(e) => match e.kind() {
+      ErrorKind::NotFound => return None,
+      e => {
+        println!("Warning: Cannot read file \"package.json\": {e}");
+        return None;
+      }
+    },
+  };
+  let reader = BufReader::new(file);
+  match serde_json::from_reader(reader) {
+    Ok(content) => Some(content),
+    Err(e) => {
+      println!("Warning: file \"package.json\" has an invalid structure: {e}",);
+      None
     }
+  }
 }
 
 fn parse_scripts(package_json: PackageJson) -> Vec<Task> {
-    let mut result = vec![];
-    for (key, value) in package_json.scripts {
-        result.push(Task {
-            name: key.clone(),
-            cmd: S("npm"),
-            argv: vec![S("run"), key, S("--silent")],
-            desc: value,
-        });
-    }
-    result.sort_unstable_by(Task::sort);
-    result
+  let mut result = vec![];
+  for (key, value) in package_json.scripts {
+    result.push(Task {
+      name: key.clone(),
+      cmd: S("npm"),
+      argv: vec![S("run"), key, S("--silent")],
+      desc: value,
+    });
+  }
+  result.sort_unstable_by(Task::sort);
+  result
 }
--- src/stacks/rust_cargo.rs
+++ src/stacks/rust_cargo.rs
@@ -5,49 +5,49 @@ use std::path::Path;
 use std::process::Command;
 
 struct RustCargoStack {
-    tasks: Vec<Task>,
+  tasks: Vec<Task>,
 }
 
 impl Display for RustCargoStack {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str("Rust (Cargo)")
-    }
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    f.write_str("Rust (Cargo)")
+  }
 }
 
 impl Stack for RustCargoStack {
-    fn setup(&self) -> Option<Command> {
-        None
-    }
+  fn setup(&self) -> Option<Command> {
+    None
+  }
 
-    fn tasks(&self) -> &Vec<Task> {
-        &self.tasks
-    }
+  fn tasks(&self) -> &Vec<Task> {
+    &self.tasks
+  }
 }
 
 pub fn scan(stacks: &mut Stacks) {
-    if !Path::new("Cargo.lock").exists() {
-        return;
-    }
-    stacks.push(Box::new(RustCargoStack {
-        tasks: vec![
-            Task {
-                name: S("build"),
-                cmd: S("cargo"),
-                argv: vec![S("build")],
-                desc: S("cargo build"),
-            },
-            Task {
-                name: S("check"),
-                cmd: S("cargo"),
-                argv: vec![S("check")],
-                desc: S("cargo check"),
-            },
-            Task {
-                name: S("test"),
-                cmd: S("cargo"),
-                argv: vec![S("test")],
-                desc: S("cargo test"),
-            },
-        ],
-    }));
+  if !Path::new("Cargo.lock").exists() {
+    return;
+  }
+  stacks.push(Box::new(RustCargoStack {
+    tasks: vec![
+      Task {
+        name: S("build"),
+        cmd: S("cargo"),
+        argv: vec![S("build")],
+        desc: S("cargo build"),
+      },
+      Task {
+        name: S("check"),
+        cmd: S("cargo"),
+        argv: vec![S("check")],
+        desc: S("cargo check"),
+      },
+      Task {
+        name: S("test"),
+        cmd: S("cargo"),
+        argv: vec![S("test")],
+        desc: S("cargo test"),
+      },
+    ],
+  }));
 }
