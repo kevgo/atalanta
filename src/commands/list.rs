416e3e33cb8b436e44152af9611ecc30fcdf99a1
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-13 10:55:38 -0600

    Remove unsafe code

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,7 +1,7 @@
 use crate::domain::{Outcome, Task, Workspace};
 use ansi_term::Style;
+use std::io;
 use std::io::Write;
-use std::str;
 use tabwriter::TabWriter;
 
 /// lists all available commands
@@ -14,17 +14,17 @@ pub fn list(workspace: Workspace) -> Outcome {
 }
 
 pub fn print_stack(tasks: &Vec<Task>) {
-  let mut tw = TabWriter::new(vec![]);
+  let mut tab_writer = TabWriter::new(vec![]);
   for task in tasks {
     let text = format!(
       "  {}\t{}\n",
       Style::new().bold().paint(&task.name),
       task.desc
     );
-    _ = tw.write(text.as_bytes()).unwrap();
+    _ = tab_writer.write(text.as_bytes()).unwrap();
   }
-  let bytes = tw.into_inner().unwrap();
-  unsafe {
-    println!("{}", str::from_utf8_unchecked(&bytes));
-  }
+  let bytes = tab_writer.into_inner().unwrap();
+  let mut stdout = io::stdout();
+  let _ = stdout.write(&bytes);
+  let _ = stdout.write(&[10]);
 }
