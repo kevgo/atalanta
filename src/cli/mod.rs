eda70694ebeb3c397353e61673fe4089a541d7fe
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-25 16:48:51 -0600

    mod.rs print_stacks.rs

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,9 +3,9 @@
 mod dialog;
 mod exit_code;
 mod parse;
-mod print_tasks;
+mod print_stacks;
 
 pub(crate) use dialog::select;
 pub(crate) use exit_code::exit_status_to_code;
 pub(crate) use parse::parse;
-pub(crate) use print_tasks::print_stacks;
+pub(crate) use print_stacks::print_stacks;
