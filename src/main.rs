b95a63445a0a842d61afae77bc999d12fd0f9849
Kevin Goslar <kevin.goslar@gmail.com>
2025-02-25 09:09:56 -0600

    main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use domain::{Command, Outcome};
 
 fn main() -> Outcome {
   match cli::parse(std::env::args()) {
-    Command::Run(name) => commands::run(Workspace::load(), name),
-    Command::Setup => commands::setup(Workspace::load()),
+    Command::Run(name) => commands::run(stacks::load(), name),
+    Command::Setup => commands::setup(stacks::load()),
     Command::FishCompletionSetup => commands::completions::fish::print(),
     Command::FishCompletion => commands::completions::fish::tasks(stacks::load()),
   }
