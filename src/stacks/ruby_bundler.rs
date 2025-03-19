4280ace2660dc741d5e0a5d7fecb6b1a783bdb2b
Kevin Goslar <kevin.goslar@gmail.com>
2025-03-18 20:02:44 -0500

    task.rs ruby_bundler.rs

--- src/stacks/ruby_bundler.rs
+++ src/stacks/ruby_bundler.rs
@@ -57,23 +57,30 @@ mod tests {
   #[test]
   fn parse_task_list() {
     let give = "\
-rake build            # Build kappamaki-1.0.0.gem into the pkg directory
-rake build:checksum   # Generate SHA512 checksum of kappamaki-1.0.0.gem into the checksums directory
-rake clean            # Remove any temporary products
-rake clobber          # Remove any generated files
-rake install          # Build and install kappamaki-1.0.0.gem into system gems
-rake install:local    # Build and install kappamaki-1.0.0.gem into system gems without network access
-rake lint             # Run linter
+rake  build:checksum   # Generate SHA512 checksum of kappamaki-1.0.0.gem into the checksums directory
 rake release[remote]  # Create tag v1.0.0 and build and push kappamaki-1.0.0.gem to rubygems.org
 rake spec             # Run RSpec code examples
 ";
     let have = super::parse_task_list(give);
-    let want = Tasks::new();
+    let mut want = Tasks::new();
     want.push(Task {
-      name: S("build"),
+      name: S("build:checksum"),
       cmd: S("bundle"),
-      argv: vec![S("exec"), S("rake"), S("build")],
-      desc: S(""),
+      argv: vec![S("exec"), S("rake"), S("build:checksum")],
+      desc: S("Generate SHA512 checksum of kappamaki-1.0.0.gem into the checksums directory"),
     });
+    want.push(Task {
+      name: S("release[remote]"),
+      cmd: S("bundle"),
+      argv: vec![S("exec"), S("rake"), S("release[remote]")],
+      desc: S("Create tag v1.0.0 and build and push kappamaki-1.0.0.gem to rubygems.org"),
+    });
+    want.push(Task {
+      name: S("spec"),
+      cmd: S("bundle"),
+      argv: vec![S("exec"), S("rake"), S("spec")],
+      desc: S("Run RSpec code examples"),
+    });
+    assert_eq!(have, want);
   }
 }
